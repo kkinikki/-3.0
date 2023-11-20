8f1d31f350bdf6d2508e97677f6950a973ac90b6
kkinikki <polina.nov77@mail.ru>
2023-11-20 13:07:26 +0300

    Добавьте файлы проекта.

---
+++ задание 3.0/задание 3.0.cpp
@@ -0,0 +1,57 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+
+	int mas[10];
+	mas[0] = 0;
+	mas[1] = 1;
+	mas[2] = 2;
+	mas[3] = 3;
+	mas[4] = 4;
+	mas[5] = 5;
+	mas[6] = 6;
+	mas[7] = 7;
+	mas[8] = 8;
+	mas[9] = 9;
+
+	int sum = 0;
+
+	for (int i = 0; i <= 9; i++) {
+		if (i % 2 != 0);
+		sum += i;
+	}
+	cout << "Сумма нечетных чисел:" << endl;
+	cout << sum;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+
+	int mas[10];
+	mas[0] = 0;
+	mas[1] = 1;
+	mas[2] = 2;
+	mas[3] = 3;
+	mas[4] = 4;
+	mas[5] = 5;
+	mas[6] = 6;
+	mas[7] = 7;
+	mas[8] = 8;
+	mas[9] = 9;
+
+	int sum = 0;
+
+	for (int i = 0; i <= 9; i++) {
+		if (i % 2 != 0);
+		sum += i;
+	}
+	cout << "Сумма нечетных чисел:" << endl;
+	cout << sum;
+}
